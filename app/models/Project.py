4981cc3cb5490123cf2eb12ba25f3f717b6903b0
Martin <martindell.oro@gmail.com>
2020-07-03 11:28:14 -0300

    Project

---
+++ app/models/Project.py
@@ -0,0 +1,14 @@
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtSql import QSqlTableModel
+from PyQt5.QtCore import Qt
+from ..lib.Store import Store
+
+class Project(QSqlTableModel):
+    
+    def __init__(self, *args, db=Store().getDB(), **kwargs):        
+        super(Test, self).__init__(*args, **kwargs)
+        self.setTable("projects")
+        self.setEditStrategy(QSqlTableModel.OnFieldChange)
+        self.select()
+        self.setHeaderData(0, Qt.Horizontal, "ID")
+        self.setHeaderData(1, Qt.Horizontal, "task")        
