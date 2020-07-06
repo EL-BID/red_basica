c3febba5bfefbe736ecddffc1d6fbe9aabe814cf
Martin <martindell.oro@gmail.com>
2020-07-06 18:00:55 -0300

    compiling

--- app/resources/compile.sh
+++ app/resources/compile.sh
@@ -5,7 +5,6 @@ pyuic5 project_dialog.ui -o ../views/ProjectDialogUi.py
 pyuic5 new_project_dialog.ui -o ../views/NewProjectDialogUi.py
 pyuic5 parameter_dialog.ui -o ../views/ParameterDialogUi.py
 pyrcc5 images/contributions.qrc -o ../views/contributions_rc.py
-sed -i 's/import\ contributions_rc/from\ .\ import\ contributions_rc/g' ../views/ParameterDialogUi.py 
+sed -i 's/import\ contributions_rc/from\ .\ import\ contributions_rc/g' ../views/ParameterDialogUi.py # diff between python 2.7 and 3.4
 echo " "
 echo "done!"
-# echo "Remember to change the import contributions_rc in ParameterDialogUi to from . import contributions_rc (diff between python 2.7 and 3.4)"
