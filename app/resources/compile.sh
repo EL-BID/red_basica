4324f29ce96adda78687e35c433ee6e10c8792c9
Fede Sanchez <federico.augusto.sanchez@gmail.com>
2020-07-06 16:45:27 -0300

    compile script,  to generate views

---
+++ app/resources/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pyuic5 main_window.ui -o ../views/MainWindowUi.py
+pyuic5 project_dialog.ui -o ../views/ProjectDialogUi.py
+pyuic5 new_project_dialog.ui -o ../views/NewProjectDialogUi.py
+pyuic5 parameter_dialog.ui -o ../views/ParameterDialogUi.py
+pyrcc5 images/contributions.qrc -o ../views/contributions_rc.py
+
+echo " "
+echo "listo!"
+echo "Recordar cambiar el import contributions_rc en ParameterDialogUi por from . import contributions_rc"
