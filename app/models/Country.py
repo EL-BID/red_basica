51447485fc53e0d43b9982fa0ad484564e28272c
Martin <martindell.oro@gmail.com>
2020-07-15 12:42:54 -0300

    Adding language on queries

--- app/models/Country.py
+++ app/models/Country.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import pyqtSignal, QModelIndex
+from PyQt5.QtCore import pyqtSignal, QModelIndex, QLocale
 from PyQt5.QtSql import QSqlTableModel, QSqlQuery
 from PyQt5.QtWidgets import QCompleter
 from PyQt5.QtCore import Qt
@@ -9,6 +9,8 @@ class Country(QSqlTableModel):
     def __init__(self, *args, db=Store().getDB(), **kwargs):        
         super(Country, self).__init__(*args, **kwargs)
         self.setTable("countries")
+        self.locale = QLocale().name()
+        self.language = self.locale[0:2] if self.locale[0:2] in ('en','es','pt') else 'en' 
         self.nameFieldIndex = self.fieldIndex('name_es')
         self.index = self.fieldIndex('id')
         self.setSort(self.nameFieldIndex, Qt.AscendingOrder)        
@@ -21,7 +23,7 @@ class Country(QSqlTableModel):
         countries_list = []
         for i in range(countryModel.rowCount()):
             _id = countryModel.record(i).value("id")
-            name = countryModel.record(i).value("name_es")
+            name = countryModel.record(i).value("name_" + self.language)
             countries_list.append((_id, name))
         return countries_list
 
