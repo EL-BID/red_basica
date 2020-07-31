c8b5bf629a0910bd80f59be199dbb88f00b99eb1
Martin <martindell.oro@gmail.com>
2020-07-31 10:25:42 -0300

    Removing store in model

--- app/models/WaterLevelAdj.py
+++ app/models/WaterLevelAdj.py
@@ -1,11 +1,10 @@
 from PyQt5.QtCore import pyqtSignal, QModelIndex
-from PyQt5.QtSql import QSqlRelation, QSqlRelationalTableModel, QSqlTableModel, QSqlQuery
+from PyQt5.QtSql import QSqlRelation, QSqlRelationalTableModel
 from PyQt5.QtCore import Qt
-from ..lib.Store import Store
 
 class WaterLevelAdj(QSqlRelationalTableModel):
     
-    def __init__(self, *args, db=Store().getDB(), **kwargs):
+    def __init__(self, *args, **kwargs):
         super(WaterLevelAdj, self).__init__(*args, **kwargs)
         self.setTable("wl_adj")
         self.select()
