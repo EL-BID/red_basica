7326788659c01c6f05f60443a1956768143976a3
Martin <martindell.oro@gmail.com>
2020-10-21 16:01:16 -0300

    Compiling

--- app/resources/compile.sh
+++ app/resources/compile.sh
@@ -2,6 +2,7 @@
 
 pyuic5 main_window.ui -o ../views/ui/MainWindowUi.py
 pyuic5 edit_values_dialog.ui -o ../views/ui/EditValuesDialogUi.py
+pyuic5 iterations.ui -o ../views/ui/IterationsDialogUi.py
 pyuic5 project_dialog.ui -o ../views/ui/ProjectDialogUi.py
 pyuic5 new_project_dialog.ui -o ../views/ui/NewProjectDialogUi.py
 pyuic5 parameter_dialog.ui -o ../views/ui/ParameterDialogUi.py
