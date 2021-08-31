84b3fc631c2c118f4a20c2b96fb775f0cd494b0c
martin <martindell.oro@gmail.com>
2021-08-31 17:05:38 -0300

    Adding naDifNeeded

--- app/views/MainView.py
+++ app/views/MainView.py
@@ -483,6 +483,7 @@ class MainView(QMainWindow, Ui_MainWindow):
         self.wlaModel.select()
         while self.wlaModel.canFetchMore():
             self.wlaModel.fetchMore()
+        self.maxNaLabel.setText(str(self.wlaModel.getMaxNaDiffNeeded()))
 
     def startImport(self, validate=False, only_selected_features=False):
         checked = validate and self._dialogs["parameters"].is_valid_form()
