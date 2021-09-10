9b1858e037db7dc5fee0853ab200b1f0b7b2e190
Fede <federico.augusto.sanchez@gmail.com>
2021-09-10 16:33:24 -0300

    adding devices and fixing x-axis issue

--- profile/views/MainView.py
+++ profile/views/MainView.py
@@ -2,7 +2,7 @@ from math import floor
 from .ui.ProfileWidgetUi import Ui_ProfileWidget
 from qgis.PyQt.QtWidgets import QDockWidget
 from qgis.PyQt.QtCore import *
-from qgis.core import QgsProject, QgsWkbTypes, QgsPointXY, QgsRasterLayer, QgsRaster
+from qgis.core import QgsProject, QgsWkbTypes, QgsPointXY, QgsRasterLayer, QgsRaster, QgsDistanceArea
 from ...base.helper_functions import HelperFunctions
 from .. import pyqtgraph as pg
 from ...app.models.Calculation import Calculation
@@ -22,6 +22,8 @@ class MainView(QDockWidget, Ui_ProfileWidget):
         self.layout = self.frame_for_plot.layout()
         self.layers = {}
         self.area_fill = None
+        self.area_fill_margin = 1
+        self.devices = None
         self.plotWdg = self.set_plot_widget()
         self.layout.addWidget(self.plotWdg)
 
@@ -43,6 +45,8 @@ class MainView(QDockWidget, Ui_ProfileWidget):
     def clearLayers(self):
         if self.area_fill:
             self.plotWdg.removeItem(self.area_fill)
+        if self.devices:
+            self.plotWdg.removeItem(self.devices)
         for k in self.layers.keys():
             self.layers[k].clear()
 
@@ -77,48 +81,72 @@ class MainView(QDockWidget, Ui_ProfileWidget):
         yRaster = []
         y = []
         x = []
-        xVal = 0
-        for i in features:
-            col_seg = i.attribute(col_seg_att_name)
-            line = i.geometry()
-            #ground layer
-            for part in line.get():
-                line_start = part[0]
-                line_end = part[-1]
-                pointm = virtualLayer.diff(line_end, line_start)
-                cosa,cosb = virtualLayer.dirCos(pointm)
-                lg = virtualLayer.length(line_end, line_start)
-                for i in range(interval, int(lg) ,interval):
-                    point = QgsPointXY(line_start.x()  + (i * cosa), line_start.y() + (i*cosb))
-                    ident = rasterLayer.dataProvider().identify(point, QgsRaster.IdentifyFormatValue)
-                    virtualLayer.createPoint(point)
-                    yVal = list(ident.results().values())[0]
-                    xVal = 0 if not xRaster else (xVal + interval)
-                    yRaster.append(yVal)
-                    xRaster.append(xVal)
-            #pipes layer
+        devices = {'x':[], 'y':[], 'h':[] }
+        xVal = None
+        for f in features:
+            col_seg = f.attribute(col_seg_att_name)
+            line = f.geometry()
+
+            #overlays
             data = Calculation.getActiveProfileData(col_seg)
             for n in data.keys():
                 for col in data[n]:
-                    x.append(0 if col['x_initial'] == None else col['x_initial'])
-                    x.append(col['x_final'])
-                for col in data[n]:
+                    #pipes layer
+                    x1 = 0 if col['x_initial'] == None else col['x_initial']
+                    if xVal is None:
+                        xVal = x1
+                    x2 = col['x_final']
+                    x.extend([x1, x2]) 
+                    
                     initialPointY = QgsPointXY(col['geom_x_initial'], col['geom_y_initial'])
                     iPy = rasterLayer.dataProvider().identify(initialPointY, QgsRaster.IdentifyFormatValue)
                     finalPointY = QgsPointXY(col['geom_x_final'], col['geom_y_final'])
                     fPy = rasterLayer.dataProvider().identify(finalPointY, QgsRaster.IdentifyFormatValue)
-                    y.append(list(iPy.results().values())[0] - col['y_initial'])
-                    y.append(list(fPy.results().values())[0] - col['y_final'])
+                    y1 = list(iPy.results().values())[0] - col['y_initial'] 
+                    y2 = list(fPy.results().values())[0] - col['y_final']
+                    y.extend([y1, y2])
+                    
+                    #devices
+                    devices['x'].extend([x1, x2])
+                    h1 = col['y_initial']
+                    h2 = col['y_final']
+                    devices['h'].extend([h1, h2])                                       
+                    devices['y'].extend([y1 + h1/2, y2 + h2/2])
+
+            #ground layer            
+            for part in line.get():
+                line_start = part[0]
+                line_end = part[-1]                
+                pointm = virtualLayer.diff(line_end, line_start)
+                cosa,cosb = virtualLayer.dirCos(pointm)
+                lg = virtualLayer.length(line_end, line_start)                
+                distance = 0
+                i = 0                
+                while distance < (lg + self.area_fill_margin):
+                    i += interval
+                    point = QgsPointXY(line_start.x()  + (i * cosa), line_start.y() + (i * cosb))
+                    ident = rasterLayer.dataProvider().identify(point, QgsRaster.IdentifyFormatValue)
+                    virtualLayer.createPoint(point)
+                    yVal = list(ident.results().values())[0]
+                    xVal = xVal if not xRaster else (xVal + interval)                    
+                    yRaster.append(yVal)
+                    xRaster.append(xVal)                   
+                    distance += interval 
 
         #TODO; another button to activate the new point layer
         #virtualLayer.displayLayer
 
         #draw ground area
         self.layers['ground'] = self.plotWdg.plot(xRaster, yRaster, pen=pg.mkPen( 'CCCCCC',  width=1))
-        yGroundBase = [ (min(y) - 3) for i in y]
-        self.layers['ground_base'] = self.plotWdg.plot(x, yGroundBase, pen=pg.mkPen( 'CCCCCC',  width=1))
+        yGroundBase = [ (min(yRaster) - self.area_fill_margin) for i in yRaster]
+        self.layers['ground_base'] = self.plotWdg.plot(xRaster, yGroundBase, pen=pg.mkPen( 'CCCCCC',  width=1))
         self.area_fill = pg.FillBetweenItem(self.layers['ground'], self.layers['ground_base'], brush=pg.mkBrush(248, 214, 179, 100))
         self.plotWdg.addItem(self.area_fill)
+        
+        #draw inspection devices
+        self.devices = pg.BarGraphItem(x = devices['x'], y = devices['y'], height = devices['h'], width = 0.6, brush ='w')
+        self.plotWdg.addItem(self.devices) 
+
         #draw pipes
-        self.layers['pipes'] = self.plotWdg.plot(x, y, pen=pg.mkPen( 'g',  width=3), symbol='o')
-        self.plotWdg.getViewBox().autoRange(items=self.plotWdg.getPlotItem().listDataItems())
+        self.layers['pipes'] = self.plotWdg.plot(x, y, pen=pg.mkPen( 'g',  width=3), symbol='o')        
+        self.plotWdg.getViewBox().autoRange(items=self.plotWdg.getPlotItem().listDataItems())        
